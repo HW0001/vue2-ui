cfa29b5106505302fd0b4ecc33e8f7fc059e4a85
HW0001 <1600860624@qq.com>
2020-11-28 18:26:02 +0800

    增加单元测试

--- src/app.js
+++ src/app.js
@@ -10,3 +10,32 @@ Vue.component("g-button-group", GButtonGroup);
 new Vue({
   el: "#app",
 });
+
+import chai from "chai";
+var expect = chai.expect;
+
+{
+  var constructor = Vue.extend(GButton);
+  const vm = new constructor({
+    propsData: {
+      icon: "setting",
+    },
+  });
+  vm.$mount();
+  const icon = vm.$el.querySelector("use").getAttribute("xlink:href");
+  expect(icon).to.eq("#i-setting");
+}
+
+{
+  var constructor = Vue.extend(GButton);
+  const vm = new constructor({
+    propsData: {
+      icon: "setting",
+    },
+  });
+  vm.$on("click", function () {
+    console.log(2);
+  });
+  vm.$mount("#test");
+  vm.$el.click();
+}
