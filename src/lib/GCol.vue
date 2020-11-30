ee10c31cf396ac468ceeb0235020924143a1451d
HW0001 <1600860624@qq.com>
2020-11-30 22:16:12 +0800

    增加offset和gutter属性

--- src/lib/GCol.vue
+++ src/lib/GCol.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="g-col" :class="{ [`span_${span}`]: span }">
-    <slot />
+  <div
+    class="g-col"
+    :class="{ [`span_${span}`]: span, [`offset_${offset}`]: offset }"
+    :style="{ paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px' }"
+  >
+    <div style=" border: 1px solid blue;"><slot /></div>
   </div>
 </template>
 <script>
@@ -8,18 +12,29 @@ export default {
   name: "GCol",
   props: {
     span: [Number, String],
+    offset: [Number, String],
+  },
+  data() {
+    return {
+      gutter: 0,
+    };
   },
 };
 </script>
 <style lang="scss" scoped>
 .g-col {
   width: 50%;
-  border: 1px solid gray;
 }
 $class-prefix: span_;
 @for $n from 1 through 24 {
   .#{$class-prefix}#{$n} {
     width: ($n/24) * 100%;
+  }
+}
+$class-prefix: offset_;
+@for $n from 1 through 24 {
+  .#{$class-prefix}#{$n} {
+    margin-left: ($n/24) * 100%;
   }
 }
 </style>
--- src/lib/GRow.vue
+++ src/lib/GRow.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="g-row">
+  <div
+    class="g-row"
+    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
+  >
     <slot />
   </div>
 </template>
+<script>
+export default {
+  name: "GRow",
+  props: {
+    gutter: [Number, String],
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
+  },
+};
+</script>
 <style lang="scss" scoped>
 .g-row {
   display: flex;
+  margin-bottom: 20px;
 }
 </style>
