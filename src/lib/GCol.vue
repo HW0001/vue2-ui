127009ba2b3c3c7bcee45b0c2b034d5432f73fda
HW0001 <1600860624@qq.com>
2020-12-01 22:45:22 +0800

    增加响应式布局基础功能

--- src/lib/GCol.vue
+++ src/lib/GCol.vue
@@ -9,6 +9,12 @@ export default {
   props: {
     span: [Number, String],
     offset: [Number, String],
+    xs: { type: Object },
+    sm: { type: Object },
+    md: { type: Object },
+    lg: { type: Object },
+    xl: { type: Object },
+    xxl: { type: Object },
   },
   data() {
     return {
@@ -17,8 +23,17 @@ export default {
   },
   computed: {
     colClass() {
-      const { span, offset } = this;
-      return { [`span_${span}`]: span, [`offset_${offset}`]: offset };
+      const { span, offset, xs, sm, md, lg, xl, xxl } = this;
+      return [
+        span && `span_${span}`,
+        offset && `offset_${offset}`,
+        xs && xs.span && "col_xs_" + xs.span,
+        sm && sm.span && "col_sm_" + sm.span,
+        md && md.span && "col_md_" + md.span,
+        lg && lg.span && "col_lg_" + lg.span,
+        xl && xl.span && "col_xl_" + xl.span,
+        xxl && xxl.span && "col_xxl_" + xxl.span,
+      ];
     },
     colStyle() {
       const { gutter } = this;
@@ -32,7 +47,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 .g-col {
-  width: 50%;
 }
 $class-prefix: span_;
 @for $n from 1 through 24 {
@@ -44,6 +58,46 @@ $class-prefix: offset_;
 @for $n from 1 through 24 {
   .#{$class-prefix}#{$n} {
     margin-left: ($n/24) * 100%;
+  }
+  @media (max-width: 576px) {
+    $class-prefix: col_xs_;
+    @for $n from 1 through 24 {
+      .#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col_sm_;
+    @for $n from 1 through 24 {
+      .#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col_xl_;
+    @for $n from 1 through 24 {
+      .#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col_xl_;
+    @for $n from 1 through 24 {
+      .#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201px) {
+    $class-prefix: col_xxl_;
+    @for $n from 1 through 24 {
+      .#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
   }
 }
 </style>
