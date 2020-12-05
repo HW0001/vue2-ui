4fe4e1094cebd22ff9fd378dfbcf0115821a4994
HW0001 <1600860624@qq.com>
2020-12-05 20:53:06 +0800

    增加side切换动画

--- src/lib/GSider.vue
+++ src/lib/GSider.vue
@@ -1,15 +1,43 @@
 <template>
-  <aside class="g-sider">
-    <slot />
-  </aside>
+  <transition name="fade">
+    <aside class="g-sider" v-if="visiable">
+      <slot></slot>
+      <button @click="toggle">关闭</button>
+    </aside>
+  </transition>
 </template>
 <script>
 export default {
   name: "GSider",
+  data() {
+    return {
+      visiable: true,
+    };
+  },
+  methods: {
+    toggle() {
+      this.visiable = false;
+    },
+  },
 };
 </script>
 <style lang="scss" scope>
 .g-sider {
+  width: 200px;
   border: 1px solid red;
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  margin-left: -180px;
 }
 </style>
