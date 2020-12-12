fc2f80196856e8bea2bd8d8904fdd8ce46e9a8e9
HW0001 <1600860624@qq.com>
2020-12-12 21:29:02 +0800

    增加创建toast

--- src/lib/GToast-plugin.js
+++ src/lib/GToast-plugin.js
@@ -1,7 +1,14 @@
+import GToast from "./GToast";
 export default {
   install(Vue) {
     Vue.prototype.$toast = function(message) {
-      console.log(message);
+      const Constructor = Vue.extend(GToast);
+      const vm = new Constructor();
+      console.log(vm);
+      vm.$slots.default = [message];
+      const div = document.createElement("div");
+      document.body.appendChild(div);
+      vm.$mount(div);
     };
   },
 };
