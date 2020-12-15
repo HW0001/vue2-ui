a0e6699ae77c51ce67c1e38cbf90c0f6d8d1132f
HW0001 <1600860624@qq.com>
2020-12-15 16:32:50 +0800

    完善toast props测试用例

--- test/toast.test.js
+++ test/toast.test.js
@@ -9,18 +9,83 @@ describe("GToast", () => {
   it("存在.", () => {
     expect(GToast).to.be.ok;
   });
-  describe("props",(done)=>{
-    const Comstructor = Vue.extend(GToast)
-    const vm = new Comstructor({
-        propsData:{
-            autoClose:true,
-            autoCloseDelay:2
-        }
-    })
-    vm.$mount()
-    setTimeout(()=>{
-        expect(vm.$el).to.be.null
-        done()
-    },3)
-  })
+  describe("props", () => {
+    const Comstructor = Vue.extend(GToast);
+    it("autoClose 和 autoCloseDelay", (done) => {
+      const vm = new Comstructor({
+        propsData: {
+          autoClose: true,
+          autoCloseDelay: 1,
+        },
+      });
+      const div =  document.createElement("div")
+      document.body.append(div)
+      vm.$mount(div);
+      setTimeout(() => {
+        expect(document.body.contains(vm.$el)).to.eq(true); 
+        done();
+      }, 1500);
+    });
+
+    it("showClose", () => {
+        const vm = new Comstructor({
+          propsData: {
+            showClose: true,
+          },
+        });
+        const div =  document.createElement("div")
+        document.body.append(div)
+        vm.$mount(div);
+        expect(vm.$el.querySelector("use").getAttribute("xlink:href")).to.eq("#i-close")
+      });
+
+      it("使用html", () => {
+        const vm = new Comstructor({
+          propsData: {
+            useHtml: true,
+          },
+        });
+        vm.$slots.default=["<a>a标签</a>"]
+        const div =  document.createElement("div")
+        document.body.append(div)
+        vm.$mount(div);
+        expect(vm.$el.querySelector("a")).to.be.ok
+      });
+
+      it("position", () => {
+        const vm = new Comstructor({
+          propsData: {
+            position: "top",
+          },
+        }); 
+        const div =  document.createElement("div")
+        document.body.append(div)
+        vm.$mount(div);
+        expect(vm.$el.classList.contains("g-toast-position-top")).to.eq(true)
+      });
+
+      it("position", () => {
+        const vm = new Comstructor({
+          propsData: {
+            position: "middle",
+          },
+        }); 
+        const div =  document.createElement("div")
+        document.body.append(div)
+        vm.$mount(div);
+        expect(vm.$el.classList.contains("g-toast-position-middle")).to.eq(true)
+      });
+
+      it("position", () => {
+        const vm = new Comstructor({
+          propsData: {
+            position: "bottom",
+          },
+        }); 
+        const div =  document.createElement("div")
+        document.body.append(div)
+        vm.$mount(div);
+        expect(vm.$el.classList.contains("g-toast-position-bottom")).to.eq(true)
+      });
+  });
 });
