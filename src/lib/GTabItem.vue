594109d6af7f03de89988595f71e7177a8df1f53
HW0001 <1600860624@qq.com>
2020-12-29 10:02:37 +0800

    创建Tabs和Tabitem组件

---
+++ src/lib/GTabItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <slot></slot>
+        <div></div>
+    </div>
+</template>
+<script>
+export default {
+    name:"GTabs",
+    mounted(){
+        console.log(this)
+    }
+}
+</script>
