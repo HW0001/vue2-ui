1b326a75236c8630acaf7632254afcf50f14f22e
HW0001 <1600860624@qq.com>
2020-12-31 15:12:40 +0800

    完善测试用例

--- test/tabs.test.js
+++ test/tabs.test.js
@@ -19,20 +19,37 @@ describe("GTabs && GTabItem", () => {
   const TabConstructer = Vue.extend(GTabs)
   const ItemConstructer = Vue.extend(GTabItem)
   describe("props", () => {
-        it("title",()=>{
-          const div = document.createElement("div")
-          document.body.appendChild(div)
-          div.innerHTML=`
-                <g-tabs active-key.sync="2">
-                <g-tab-item title='一www' item-key='1'>测试1</g-tab-item>
-                <g-tab-item title='二的' item-key='2'>测试2</g-tab-item>
-                <g-tab-item title='三是是是' item-key='3'>测试3</g-tab-item>
-                </g-tabs>
-          `
-          const vm = new Vue({
-              el:div
-          })
-          console.log(vm.$el.innerHTML)
+        const div = document.createElement("div")
+        document.body.appendChild(div)
+        div.innerHTML=`
+            <g-tabs active-key="2">
+            <g-tab-item title='一www' item-key='1' disabled>测试1</g-tab-item>
+            <g-tab-item title='二的' item-key='2'>测试2</g-tab-item>
+            <g-tab-item title='三是是是' item-key='3'>测试3</g-tab-item>
+            </g-tabs>
+        `
+        const vm = new Vue({
+            el:div
         })
+        it("active-key",(done)=>{
+          vm.$nextTick(()=>{
+             expect(vm.$el.querySelector("span:nth-child(2)").classList.contains("selected")).to.be.true
+             done()
+            })
+        })
+        it("title",(done)=>{
+            vm.$nextTick(()=>{
+               expect(vm.$el.querySelectorAll("span").length).to.be.eq(3)
+               done()
+              })
+          })
+          it("disabled",(done)=>{
+            vm.$nextTick(()=>{
+               expect(vm.$el.querySelector("span").classList.contains("disabled")).to.be.true
+               vm.$el.remove()
+               vm.$destroy()
+               done()
+              })
+          })
   });
 });
