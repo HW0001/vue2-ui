6f2e723a107000d438fa04c04f340e4964260f0b
HW0001 <1600860624@qq.com>
2021-01-04 16:40:40 +0800

    调整popover出现位置

--- src/lib/GPopover.vue
+++ src/lib/GPopover.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div v-if="visible"><slot name="content"></slot></div>
-    <span @click.stop="toggleVisible"><slot></slot></span>
+    <div v-if="visible" ref="contentWrapper" class="contentWrapper">
+      <slot name="content"></slot>
+    </div>
+    <span @click.stop="toggleVisible" ref="trigger"><slot></slot></span>
   </div>
 </template>
 <script>
@@ -13,19 +15,37 @@ export default {
     };
   },
   mounted() {
-    this.$nextTick(()=>{
-       const handle=()=>{
-            this.visible=false
-        }
-        document.addEventListener("click",handle)
-    })
+    this.$nextTick(() => {
+      const handle = () => {
+        this.visible = false;
+      };
+      document.addEventListener("click", handle);
+    });
   },
   methods: {
     toggleVisible() {
       this.visible = !this.visible;
+      this.$nextTick(() => {
+        if (this.visible) {
+          const el = this.$refs.contentWrapper;
+          document.body.append(el);
+          const {left,top,width,height}=this.$refs.trigger.getBoundingClientRect()
+          const {height:popheight}=el.getBoundingClientRect()
+          let poptop=top-height-10
+          if(poptop<popheight){
+              poptop=top+height+10
+          }
+          el.style.left=`${left}px`
+          el.style.top=`${poptop}px`
+        }
+      });
     },
   },
 };
 </script>
 <style lang="scss" scoped>
+.contentWrapper {
+  position: absolute;
+  background: #ffffff;
+}
 </style>
