3c591e53e5655f26be50fc1227eed27e235c5899
HW0001 <1600860624@qq.com>
2021-01-05 16:40:35 +0800

    增加position属性，并设置样式

--- src/lib/GPopover.vue
+++ src/lib/GPopover.vue
@@ -1,6 +1,11 @@
 <template>
   <div style="display: inline-block">
-    <div v-if="visible" ref="contentWrapper" class="contentWrapper">
+    <div
+      v-if="visible"
+      ref="contentWrapper"
+      class="contentWrapper"
+      :class="{ [`position-${position}`]: true }"
+    >
       <slot name="content"></slot>
     </div>
     <span @click="toggleVisible" ref="trigger" style="display: inline-block"
@@ -19,7 +24,7 @@ export default {
         return true || ["top", "bottom", "left", "right"].indexOf(value) > -1;
       },
     },
-    postion: {
+    position: {
       type: String,
       default: "top",
       validator: (value) => {
@@ -69,10 +74,19 @@ export default {
       const triggerel = this.$refs.trigger;
       document.body.append(contentel);
       const { width, height, left, top } = triggerel.getBoundingClientRect();
+      const { width:contentWidth, height:contentHegiht} = contentel.getBoundingClientRect();
       let popleft = left + window.scrollX;
       let poptop = top + window.scrollY;
-      if (this.postion === "bottom") {
+      if (this.position === "bottom") {
+        poptop = top + window.scrollY+height;
+      }else if(this.position==='left'){
+          popleft=left+window.scrollX-contentWidth
+          poptop = top + window.scrollY-(contentHegiht-height)/2;
+      }else if(this.position==='right'){
+          popleft=left+window.scrollX+width
+          poptop = top + window.scrollY-(contentHegiht-height)/2;
       }
+      console.log(contentHegiht);
       contentel.style.left = `${popleft}px`;
       contentel.style.top = `${poptop}px`;
     },
@@ -84,12 +98,10 @@ export default {
   position: absolute;
   background: #ffffff;
   z-index: 999;
-  transform: translateY(-100%);
   padding: 0.5em 1em;
   border: 1px solid rgba(0, 0, 0, 0.7);
   border-radius: 4px;
   box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25);
-  margin-top: -10px;
   max-width: 20em;
   word-wrap: break-word;
   word-break: break-all;
@@ -100,13 +112,58 @@ export default {
     position: absolute;
     border: 10px solid transparent;
   }
-  &::after {
-    border-top-color: white;
-    top: calc(100% - 1px);
+  &.position-top {
+    margin-top: -10px;
+    transform: translateY(-100%);
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
   }
-  &::before {
-    border-top-color: black;
-    top: 100%;
+  &.position-bottom {
+      margin-top: 10px;
+    &::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+    }
+    &::before {
+      border-bottom-color: black;
+      bottom: 100%;
+    }
+  }
+    &.position-left {
+        margin-left: -10px;
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    &::before {
+      border-left-color: black;
+      left: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+      &.position-right {
+          margin-left: 10px;
+    &::after {
+      border-right-color: white;
+      right: calc(100% - 1px);
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    &::before {
+      border-right-color: black;
+      right: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
   }
 }
 </style>
