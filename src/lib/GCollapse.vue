b811e7d53204ee6c7a92fd4face6f29ef03fcb50
HW0001 <1600860624@qq.com>
2021-01-13 09:24:12 +0800

    增加freedom属性，用于不受selected控制

--- src/lib/GCollapse.vue
+++ src/lib/GCollapse.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="g-collapse"  @addItem="addItem">
+  <div class="g-collapse" @addItem="addItem">
     <slot></slot>
   </div>
 </template>
@@ -8,12 +8,14 @@ import Vue from "vue";
 export default {
   name: "GCollapse",
   props: {
-    selected: {type:Array | String,default:''},
+    selected: { type: Array | String, default: "" },
     mulitple: { type: Boolean, default: false },
+    freedom: { type: Boolean, default: false },
   },
   data() {
     return {
       eventBus: new Vue(),
+      freedomKey: [],
     };
   },
   provide() {
@@ -21,37 +23,61 @@ export default {
   },
   methods: {
     removeItem(key) {
-      if (!this.mulitple) {
-        this.$emit("update:selected", key);
+      if (this.freedom) {
+        if (this.mulitple) {
+          const index = this.freedomKey.indexOf(key);
+          index > -1 && this.freedomKey.splice(index, 1);
+        } else {
+          this.freedomKey = [];
+        }
       } else {
-        const index = this.selected.indexOf(key);
-        index > -1 &&
-          this.$emit(
-            "update:selected",
-            this.selected.filter((s) => s !== key)
-          );
+        if (!this.mulitple) {
+          this.$emit("update:selected", key);
+        } else {
+          const index = this.selected.indexOf(key);
+          index > -1 &&
+            this.$emit(
+              "update:selected",
+              this.selected.filter((s) => s !== key)
+            );
+        }
       }
       this.notify();
     },
     addItem(key) {
-      if (!this.mulitple) {
-        this.$emit("update:selected", key);
+      if (this.freedom) {
+        if (this.mulitple) {
+          this.freedomKey.push(key);
+        } else {
+          this.freedomKey = [key];
+        }
       } else {
-        this.$emit("update:selected", [...this.selected, key]);
+        if (!this.mulitple) {
+          this.$emit("update:selected", key);
+        } else {
+          this.$emit("update:selected", [...this.selected, key]);
+        }
       }
       this.notify();
     },
     notify() {
       this.$nextTick(() => {
-        this.eventBus.$emit("collapseSelected", this.selected);
+        this.eventBus.$emit(
+          "collapseSelected",
+          this.freedom ? this.freedomKey : this.selected
+        );
       });
     },
   },
   mounted() {
-    if (!this.mulitple && typeof this.selected !== "string")
-      throw new Error("mulitple is true ,expect 'selected' to be a String ");
-    if (this.mulitple && !Array.isArray(this.selected))
-      throw new Error("mulitple is false ,expect 'selected' to be a Array ");
+    if (this.freedom) {
+        this.freedomKey=JSON.parse(JSON.stringify(this.selected))
+    } else {
+      if (!this.mulitple && typeof this.selected !== "string")
+        throw new Error("mulitple is true ,expect 'selected' to be a String ");
+      if (this.mulitple && !Array.isArray(this.selected))
+        throw new Error("mulitple is false ,expect 'selected' to be a Array ");
+    }
     this.eventBus.$on("removeItem", this.removeItem);
     this.eventBus.$on("addItem", this.addItem);
     this.notify();
@@ -61,7 +87,7 @@ export default {
 <style lang="scss" scoped>
 .g-collapse {
   padding: 1em 1em;
-  border: 1px solid  rgba(153,153,153,.3);;
+  border: 1px solid rgba(153, 153, 153, 0.3);
   border-radius: 4px;
   padding: 1em;
   > div:not(:first-child) {
