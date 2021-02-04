e5c8d91e526d591ada66fbe18d3c1a83f5e1dd16
HW0001 <1600860624@qq.com>
2021-02-04 16:52:00 +0800

    增加发布文件

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import GButton from "./src/lib/GButton";
 import GButtonGroup from "./src/lib/GButtonGroup";
 import GIcon from "./src/lib/GIcon";
+import GCollapse from "./src/lib/GCollapse";
+import GCollapseItem from "./src/lib/GCollapseItem";
+import GHeader from "./src/lib/GHeader";
+import GFooter from "./src/lib/GFooter";
+import GSider from "./src/lib/GSider";
+import GLayout from "./src/lib/GLayout";
+import GMain from "./src/lib/GMain";
+import GCol from "./src/lib/GCol";
+import GRow from "./src/lib/GRow";
+import GInput from "./src/lib/GInput";
+import GPopover from "./src/lib/GPopover";
+import GTabs from "./src/lib/GTabs";
+import GTabItem from "./src/lib/GTabItem";
+import GToastplugin from "./src/lib/GToast-plugin";
+import GToast from "./src/lib/GToast";
+import './src/lib/scsshelper'
 
-export { GButton, GButtonGroup, GIcon };
+export { GButton, GButtonGroup, 
+    GIcon,GCollapse,GCollapseItem,GHeader,GFooter,GSider,GLayout,
+    GMain,GCol,GRow,GInput,GPopover,GTabs,GTabItem,GToastplugin,GToast
+ };
